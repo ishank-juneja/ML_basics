c38fef02cc4ef19ddfbd4e442ecaffe244d1126e
ishank-juneja <ishankjuneja@gmail.com>
2018-08-18 18:21:41 +0530

    Completed Tree training/generation

--- regression_tree/dataset_kaggle1/main.py
+++ regression_tree/dataset_kaggle1/main.py
@@ -4,7 +4,7 @@ import sys
 import pandas as pd
 import numpy as np
 #Use anynode to avoid names
-from anytree import AnyNode
+from anytree import AnyNode, RenderTree
 
 """	CART algorithm with regression on output class label
 	main.py: A program to implement a regresion tree on a muti-attributed data-set
@@ -25,44 +25,58 @@ from anytree import AnyNode
 
 __author__ = "Ishank Juneja"
 
-#Computes mean absolute error
-#For a set of real valued data 
-def abs_error( data ):
-
-	data = data - np.mean(data)
-	data = np.absolute(data)
-	mae = np.sum(data)/np.size(data)
-
-return mae
-
-#Computes mean sqaure error
-#For a set of real valued data 
-def MSE( data ):
-	
-	data = data - np.mean(data)
-	data = np.square(data)
-	mse = np.sum(data)/np.size(data) 
-
-return mae
-
 #gets the optimal split threshold for a 
 #particular attribute an returns the 
 #corresponding error measure 
-def optimal_split_MSE( train_data ):
+def optimal_split_MSE( train_data, attr ):
 	min_MSE  = np.inf
+	thresh = train_data[attr].min()
 	split = 0
-	for ii in (1, train_data.att_name.shape[0]-2) :
-		df_top =  train_data[0:ii]
-		df_bottom = train_data[ii:]
+	df_bottom = train_data
+
+	#scope for improvement
+	#instead of boolean indexing
+	#do all operations on sorted array
+	while(True):
+		mask = train_data[attr] > thresh
+		df_bottom = train_data[mask]
+		
+		#Check continue condition
+		if(df_bottom.empty) :
+			break
+
+		df_top = train_data[~mask]
 		cur_MSE = df_top.iloc[:,-1].std() + df_bottom.iloc[:,-1].std() 
 		if(cur_MSE < min_MSE):
 			min_MSE = cur_MSE
-			split = i
-return (split, error) 
+			split = thresh
+		thresh = df_bottom[attr].min()
+	return (split, min_MSE)
 
-def optimal_split_MAE( train_data ):
+def optimal_split_MAE( train_data , attr ):
+	min_MAE  = np.inf
+	thresh = train_data[attr].min()
+	split = 0
+	df_bottom = train_data
 
-return (split, error)
+	#scope for improvement
+	#instead of boolean indexing
+	#do all operations on sorted array
+	while(True):
+		mask = train_data[attr] > thresh
+		df_bottom = train_data[mask]
+		
+		#Check continue condition
+		if(df_bottom.empty) :
+			break
+
+		df_top = train_data[~mask]
+		cur_MAE = df_top.iloc[:,-1].mad() + df_bottom.iloc[:,-1].mad() 
+		if(cur_MAE < min_MAE):
+			min_MAE = cur_MAE
+			split = thresh
+		thresh = df_bottom[attr].min()
+	return (split, min_MAE)
 
 #Scope for improvement : Avoid passing mode again and again
 def make_tree( df , mode ):
@@ -71,48 +85,54 @@ def make_tree( df , mode ):
 	if (df.shape[0] <= min_size) :
 		#get last column for class and take mean to assign label
 		this_node = AnyNode(parent = None, Leaf = True, Label = np.mean(df.iloc[:,-1]), \
-															 att = None, sp = None)
+							 att = None, sp = None, size = df.shape[0])
 		return this_node
 
 	# Start with min possible value
 	#Update as we go along
-	min_error = np.inf
-	cur_error = 0
+	min_err = np.inf
+	cur_err = 0
 	optimal_att = ''
-	split_at = 0
+	split_pt = 0
 
-	for column in df :
+	for column in df.iloc[:, :-1] :
 		#Sort values as per this column
 		#Easy to divide on the basis of 
 		#label once this is done 
-		df.sort_values(column)
-		df.reset_index(drop = True, inplace = True)
+		#df.sort_values(column, inplace = True)
+		#df.reset_index(drop = True, inplace = True)
+		print ('Processing column ', column)
 		if(mode == 0) :
-			(split, cur_error) = optimal_split_MSE(df)
+			(split, cur_err) = optimal_split_MSE(df, column)
 		else :
-			(split, cur_error) = optimal_split_MAE(df)
+			(split, cur_err) = optimal_split_MAE(df, column)
+			print(split, cur_err)
 
-		if (cur_error < min_error) :
-			min_error = cur_error
+		if (cur_err < min_err) :
+			min_err = cur_err
 			optimal_att = column
 			split_pt = split
 
-		#Parent is none for now
-		#On returning the child is linked to the parent
-		this_node = AnyNode(parent = None, Leaf = False , Label = None, \
-									att = optimal_att, sp = split_pt)	
-		
-		mask = df[optimal_att] > split_at
-		df_right = df[mask]
-		df_left = df[~mask]
+	#Parent is none for now
+	#On returning the child is linked to the parent
+	print ('Chosen values are')
+	print ('Attribute name ', optimal_att)
+	print ('Split Point', split_pt)
 
-		right_child = make_tree(df_right, mode)
-		left_child = make_tree(df_left, mode)
-		
-		right_child.parent =  this_node
-		left_child.parent =  this_node
+	this_node = AnyNode(parent = None, Leaf = False , Label = None, \
+						att = optimal_att, sp = split_pt, size = df.shape[0])	
+	
+	mask = df[optimal_att] > split_pt
+	df_right = df[mask]
+	df_left = df[~mask]
 
-return this_node
+	right_child = make_tree(df_right, mode)
+	left_child = make_tree(df_left, mode)
+	
+	right_child.parent =  this_node
+	left_child.parent =  this_node
+
+	return this_node
 
 #Main Program
 
@@ -132,19 +152,21 @@ min_size = 2*int(sys.argv[6]) + 1
 #Either Abs or MSE
 split_method = sys.argv[7]
 
-if (split_method != '--mean_squared' or split_method != '--absolute') : 
+if (split_method == '--mean_squared' or split_method == '--absolute') : 
+	if (split_method == '--mean_squared') :
+		mode = 0
+	else :
+		mode = 1
+else :
 	sys.exit('Error : Correct usage is when the last parameter is either --absolute or \
 							mean_squared--')
-elif (split_method = '--mean_squared') :
-	mode = 0
-else :
-	mode = 1
+
 
 #Initialise Tree  
 #Extend it using Recurssion
 #Decision tree model is stored in root
 root = make_tree(train_data, mode)
-
+print (RenderTree(root))
 # if (split_method = '--mean_squared') : 
 	
 # 	# Split as per MSE
