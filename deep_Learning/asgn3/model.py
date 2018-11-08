fc134b72cae5a197c91746dd28c4fd7c76378090
ishank-juneja <ishankjuneja@gmail.com>
2018-11-08 20:11:38 +0530

    Progress in FNN CLass definition

--- deep_Learning/asgn3/model.py
+++ deep_Learning/asgn3/model.py
@@ -47,36 +47,58 @@ class myNeuralNet:
 		layer_input = layer_list[-1]
 		# The output of the final layer are logits -- Un-normalized log probability values
 		layer = tf.layers.dense(layer_input, self.dim_output_data, activation = activation_fn, kernel_regularizer = regularizer_fn)
-		#Append to internal list
+		#Append final layer to internal list
 		layer_list.append(layer)		
 		
 	def setup_training(self, learn_rate):
-		# Define loss, you might want to store it as self.loss
+		# Define and store loss (cost function) as self.loss
+		# Change this to operate on the current batch
+		# Anything that updates over time in 
+		predicted = layer_list[-1]
+		self.loss =  tf.nn.softmax_cross_entropy_with_logits_v2(labels = self.y_, logits = predicted)
 		# Define the train step as self.train_step = ..., use an optimizer from tf.train and call minimize(self.loss)
-		pass
-
+		
 	def setup_metrics(self):
+		# Function to compute accuracy on Validation data set
 		# Use the predicted labels and compare them with the input labels(placeholder defined in __init__)
 		# to calculate accuracy, and store it as self.accuracy
-		pass
+		self.accuracy = <Create Flow graph on this>
 	
+	#Need to duplicate this function for various training methods
 	# you will need to add other arguments to this function as given below
-	def train(self, sess, max_epochs, batch_size, train_size, print_step = 100): # valid_size, test_size, etc
-		# Write your training part here
-		# sess is a tensorflow session, used to run the computation graph
-		# note that all the functions uptil now were just constructing the computation graph
+	def train(self, train_data, train_labels, sess, max_epochs, batch_size, train_size, print_step = 100): # valid_size, test_size, etc
+		# sess is the tensorflow session passed from main, used to run the computation graph
+		# All the functions uptil now were just constructing the computation graph
 		
-		# one 'epoch' represents that the network has seen the entire dataset once - it is just standard terminology
+		#Drops the last few rows of the training data set to make it an
+        #Integeral multiple of Batch Size 
+		train_data = train_data[:(train_size//batch_size)*batch_size]
+		train_labels = train_labels[:(train_size//batch_size)*batch_size]
+		#Split the data into batches
+        train_data_batches = np.split(train_data, train_size//batch_size)
+        #Similar split for the output labels 
+        train_labels_batches = np.split(train_labels, train_size//batch_size)
+		
+		# one 'epoch' represents that the network has seen the entire dataset once
+		# We look at the data not all at once but in Batches
 		steps_per_epoch = int(train_size/batch_size)
 		max_steps = max_epochs * steps_per_epoch
 		for step in range(max_steps):
-			# read a batch of data from the training data
-			# now run the train_step, self.loss on this batch of training data. something like :
-			_, train_loss = sess.run([self.train_step, self.loss], feed_dict={'''here, feed in your placeholders with the data you read in the previous to previous comment'''})
+			# read the current batch of data from the training data batch collection
+			self.cur_batch = train_data_batches[steps]
+			self.cur_lables = train_labels_batches[steps]
+			# Now run train_step and self.loss on this batch of training data
+			# All the inputs to the Graph must be provided using dictioinary
+			_, train_loss = sess.run([self.train_step, self.loss], feed_dict={self.x: self.cur_batch, self.y_: self.cur_lables})
+			#Check on validation Data set every 100 iterations
+			
+
+			# Fill this
 			if (step % print_step) == 0:
-				# read the validation dataset and report loss, accuracy on it by running
+				# read the entire validation dataset and report accuracy on it and loss on training dataset by running
 				val_acc, val_loss = sess.run([self.accuracy, self.loss], feed_dict={'''here, feed in your placeholders with the data you read in the comment above'''})
 				# remember that the above will give you val_acc, val_loss as numpy values and not tensors
+				#PRINT the above values
 				pass
 			# store these train_loss and validation_loss in lists/arrays, write code to plot them vs steps
 			# Above curves are *REALLY* important, they give deep insights on what's going on
--- deep_Learning/asgn3/train_mnist.py
+++ deep_Learning/asgn3/train_mnist.py
@@ -4,20 +4,27 @@ import tensorflow as tf
 import math
 import numpy as np
 
+#Import TF Helper Class
 from model import myNeuralNet
 
 # x denotes features, y denotes labels
+
+#Load training data
 xtrain = np.load('data/mnist/xtrain.npy')
 ytrain = np.load('data/mnist/ytrain.npy')
 
+#Load validation data
 xval = np.load('data/mnist/xval.npy')
 yval = np.load('data/mnist/yval.npy')
 
+#Load test data
 xtest = np.load('data/mnist/xtest.npy')
 
+#Image dimensions, 28 x 28 rolled out images 
 dim_input = 784
 dim_output = 10
 
+#Training parameters used while running TF session
 max_epochs = 10
 learn_rate = 1e-4
 batch_size = 50
@@ -27,23 +34,33 @@ train_size = len(xtrain)
 valid_size = len(xval)
 test_size = len(xtest)
 
+#Why are these used..?
 total_images = []
 total_labels = []
 
 # Create Computation Graph using Class defined in model.py
 nn_instance = myNeuralNet(dim_input, dim_output)
 #Add hidden layers  
-nn_instance.addHiddenLayer(1000, activation_fn=tf.nn.relu)
+nn_instance.addHiddenLayer(1000, activation_fn = tf.nn.relu)
 # add more hidden layers here by calling addHiddenLayer as much as you want
-# a net of depth 3 should be sufficient for most tasks
+# a net of depth 3 (3 hidden units) should be sufficient for most tasks
+
+#Add the output layer of the NN
 nn_instance.addFinalLayer()
-nn_instance.setup_training(learn_rate)
+#Setup the computational flow graph 
+#of training using the myNN class 
+nn_instance.setup_training(learn_rate)	
+#Setup the computational flow graph 
+#For error computation
 nn_instance.setup_metrics()
 
-# Training steps
+# Training steps	
 with tf.Session() as sess:
+	#Initialize all TF variables used as part of the flow graph
 	sess.run(tf.global_variables_initializer())
-	test_pred = nn_instance.train(sess, max_epochs, train_size, batch_size) # fill in other arguments as you modify the train(self, sess, ...) in model.py
+	#Using the currently running session, perform training
+	test_pred = nn_instance.train(sess, xtrain, ytrain, max_epochs, train_size, batch_size) 
+	# fill in other arguments as you modify the train(self, sess, ...) in model.py
 	# you will have to pass xtrain, ytrain, etc ... also as arguments so that you can sample batches in train() of model.py
 
 
