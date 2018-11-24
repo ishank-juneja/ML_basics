29424f808af88a761c2004dfd2e9a3d3b6b1f154
ishank-juneja <ishankjuneja@gmail.com>
2018-11-24 19:27:48 +0530

    Completed part 1 MNIST with 95% Accuracy

--- deep_Learning/asgn3/model.py
+++ deep_Learning/asgn3/model.py
@@ -1,72 +1,75 @@
+#model.py is a helper class that makes it easy to create a Neural Net in TensorFlow
+
 import tensorflow as tf
 import numpy as np
 import math
-# include any other imports that you want
+from matplotlib import pyplot as plt
 
-'''
-This file contains a class for you to implement your neural net.
-Basic function skeleton is given, and some comments to guide you are also there.
-
-You will find it convenient to look at the tensorflow API to understand what functions to use.
-'''
-
-'''
-Implement the respective functions in this class
-You might also make separate classes for separate tasks , or for separate kinds of networks (normal feed-forward / CNNs)
-'''
 class myNeuralNet:
-	# you can add/modify arguments of *ALL* functions
-	# you might also add new functions, but *NOT* remove these ones
-	def __init__(self, dim_input_data, dim_output_data): # you can add/modify arguments of this function 
-		# Using such 'self-ization', you can access these members in later functions of the class
-		# You can do such 'self-ization' on tensors also, there is no change
-		
+	def __init__(self, dim_input_data, dim_output_data):  		
 		#Member variable for dimension of Input Data Vectors (Number of features...)
 		self.dim_input_data = dim_input_data
 		#Variable for dimension of output labels
 		self.dim_output_data = dim_output_data
 		#TF Placeholder for input data   
-		self.x =  tf.placeholder(tf.float32, [None, 784])
+		self.x =  tf.placeholder(tf.float32, [None, dim_input_data])
 		#TF Placeholder for labels 
-		self.y_ = tf.placeholder(tf.float32, [None, 10])
+		self.y_ = tf.placeholder(tf.float32, [None, dim_output_data])
 		#Initilaise Container to store all the layers of the network 
-		#Initilaise with the placeholder as the first layer
+		#Initilaise it with the placeholder as the first layer
 		self.layer_list = [self.x]
 		
 	def addHiddenLayer(self, layer_dim, activation_fn = None, regularizer_fn = None):
 		#Input to current layer is output of previous layer
-		layer_input = layer_list[-1]
+		layer_input = self.layer_list[-1]
 		#Add this layer to flow graph by making connection to input
 		layer = tf.layers.dense(layer_input, layer_dim, activation = activation_fn, kernel_regularizer = regularizer_fn)
 		#Append to internal list
-		layer_list.append(layer)
+		self.layer_list.append(layer)
 
 	def addFinalLayer(self, activation_fn = None, regularizer_fn = None): 
-		# dimensionality of final layer is stored in self.dim_output_data
+		#Dimensionality of final layer is stored in self.dim_output_data
 		#Input to current layer is output of previous layer
-		layer_input = layer_list[-1]
+		layer_input = self.layer_list[-1]
 		# The output of the final layer are logits -- Un-normalized log probability values
 		layer = tf.layers.dense(layer_input, self.dim_output_data, activation = activation_fn, kernel_regularizer = regularizer_fn)
 		#Append final layer to internal list
-		layer_list.append(layer)		
-		
+		self.layer_list.append(layer)		
+
+	def eval(self):
+		#Function to predict labels once model has been trained
+		#self.logits contains unnormalized linear scale logits
+		self.logits = self.layer_list[-1]
+		#Aplying a squashing softmax gives probabilities that sum to 1
+		self.probabilities = tf.nn.softmax(self.logits)
+		#The predicted matrix in this case will be
+		self.predictions = tf.one_hot(tf.argmax(self.probabilities, dimension = 1), depth = 10)
+		#The index to be returned in case of the MNIST task 
+		self.predicted_labels = tf.argmax(self.probabilities, 1)
+
 	def setup_training(self, learn_rate):
 		# Define and store loss (cost function) as self.loss
 		# Change this to operate on the current batch
-		# Anything that updates over time in 
-		predicted = layer_list[-1]
-		self.loss =  tf.nn.softmax_cross_entropy_with_logits_v2(labels = self.y_, logits = predicted)
-		# Define the train step as self.train_step = ..., use an optimizer from tf.train and call minimize(self.loss)
-		
+		# Anything that updates over time in tensorflow is a variable
+		self.loss =  tf.nn.softmax_cross_entropy_with_logits_v2(labels = self.y_, logits = self.logits)
+		# Define the train step as self.train_step = ..., using an optimizer from tf.train and call minimize(self.loss)
+		self.train_step = tf.train.GradientDescentOptimizer(learn_rate).minimize(self.loss)
+
 	def setup_metrics(self):
 		# Function to compute accuracy on Validation data set
 		# Use the predicted labels and compare them with the input labels(placeholder defined in __init__)
 		# to calculate accuracy, and store it as self.accuracy
-		self.accuracy = <Create Flow graph on this>
+		correct_prediction = tf.equal(self.predicted_labels, tf.argmax(self.y_, 1))
+    	# Calculate accuracy
+		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+		#Accuracy accepts logits (probabilities sum = 1) as predicted labels and 
+		#internally compares with one hot encoded labels self.y_
+		#Also the function returns 2 parameters and there are some reasons 
+		#For acc_update to be run first using a sperate TF session 
+		#self.accuracy, self.acc_update = tf.metrics.accuracy(self.y_, self.predictions)
 	
-	#Need to duplicate this function for various training methods
-	# you will need to add other arguments to this function as given below
-	def train(self, train_data, train_labels, sess, max_epochs, batch_size, train_size, print_step = 100): # valid_size, test_size, etc
+	def train(self, sess, train_data, train_labels, max_epochs, batch_size, train_size, valid_data, valid_labels, valid_size, test_data, print_step = 100): # valid_size, test_size, etc
 		# sess is the tensorflow session passed from main, used to run the computation graph
 		# All the functions uptil now were just constructing the computation graph
 		
@@ -75,43 +78,64 @@ class myNeuralNet:
 		train_data = train_data[:(train_size//batch_size)*batch_size]
 		train_labels = train_labels[:(train_size//batch_size)*batch_size]
 		#Split the data into batches
-        train_data_batches = np.split(train_data, train_size//batch_size)
+		train_data_batches = np.split(train_data, train_size//batch_size)
         #Similar split for the output labels 
-        train_labels_batches = np.split(train_labels, train_size//batch_size)
-		
+		train_labels_batches = np.split(train_labels, train_size//batch_size)
+		#Read in the validation data set once and for all
+		self.valid_data = valid_data
+		self.valid_labels = valid_labels
+		#Initialise List to store validation loss
+		L_val_loss = []
+		#List to hold training loss
+		L_train_loss = [] 
+
 		# one 'epoch' represents that the network has seen the entire dataset once
 		# We look at the data not all at once but in Batches
 		steps_per_epoch = int(train_size/batch_size)
 		max_steps = max_epochs * steps_per_epoch
 		for step in range(max_steps):
 			# read the current batch of data from the training data batch collection
-			self.cur_batch = train_data_batches[steps]
-			self.cur_lables = train_labels_batches[steps]
+			#Modulo since loop back whenever 1 epoch (pass over dataset) is complete
+			self.cur_batch = train_data_batches[step%steps_per_epoch]
+			self.cur_lables = train_labels_batches[step%steps_per_epoch]
 			# Now run train_step and self.loss on this batch of training data
 			# All the inputs to the Graph must be provided using dictioinary
 			_, train_loss = sess.run([self.train_step, self.loss], feed_dict={self.x: self.cur_batch, self.y_: self.cur_lables})
-			#Check on validation Data set every 100 iterations
-			
 
-			# Fill this
+			#Check on validation Data set every 100 or so iterations
 			if (step % print_step) == 0:
-				# read the entire validation dataset and report accuracy on it and loss on training dataset by running
-				val_acc, val_loss = sess.run([self.accuracy, self.loss], feed_dict={'''here, feed in your placeholders with the data you read in the comment above'''})
-				# remember that the above will give you val_acc, val_loss as numpy values and not tensors
-				#PRINT the above values
-				pass
-			# store these train_loss and validation_loss in lists/arrays, write code to plot them vs steps
-			# Above curves are *REALLY* important, they give deep insights on what's going on
+				# Report Accuracy and loss on validation data set by running	
+				val_acc, val_loss, predictions = sess.run([self.accuracy, self.loss, self.predictions], feed_dict={self.x: self.valid_data, self.y_: self.valid_labels})
+				# Above computation results in numpy arrays, PRINT and store above values
+				net_train_loss = np.sum(train_loss)/batch_size
+				net_val_loss = np.sum(val_loss)/valid_size
+				print("Metrics after SGD Training Step: {}".format(step)) 
+				print("Training Loss is: {:.3f}".format(net_train_loss))
+				print("Validation Loss is: {:.3f}".format(net_val_loss))
+				print("Validation Accuracy is: {:.2f}% \n".format(100*val_acc))
+				L_val_loss.append(net_val_loss)
+				#Update train_loss less often for simplicity
+				L_train_loss.append(net_train_loss)
+										
 		# -- for loop ends --
-		# Now once training is done, run predictions on the test set
-		test_predictions = sess.run('''here, put something like self.predictions that you would have made somewhere''', feed_dict={'''here, feed in test dataset'''})
+
+		# Code to plot Training loss
+		train_plot = plt.figure() 	
+		plt.plot(L_train_loss)
+		plt.xlabel('Print Steps')
+		plt.ylabel('Training Loss')
+		
+		#Plot Validation loss
+		val_plot = plt.figure() 	
+		plt.plot(L_val_loss)
+		plt.xlabel('Print Steps')
+		plt.ylabel('Validation Loss')
+		plt.show()
+		
+		# Run predictions on the test set
+		self.test_data = test_data
+		test_predictions = sess.run(self.predicted_labels, feed_dict={self.x: self.test_data})
 		return test_predictions
-		# This is because we will ask you to submit test_predictions, and some marks will be based on how your net performs on these unseen instances (test set)
-		'''
-		We have done everything in train(), but
-		you might want to create another function named eval(),
-		which calculates the predictions on test instances ...
-		'''
 
 	'''
 	NOTE:
--- deep_Learning/asgn3/train_mnist.py
+++ deep_Learning/asgn3/train_mnist.py
@@ -25,7 +25,7 @@ dim_input = 784
 dim_output = 10
 
 #Training parameters used while running TF session
-max_epochs = 10
+max_epochs = 1
 learn_rate = 1e-4
 batch_size = 50
 
@@ -40,13 +40,15 @@ total_labels = []
 
 # Create Computation Graph using Class defined in model.py
 nn_instance = myNeuralNet(dim_input, dim_output)
-#Add hidden layers  
+#Add hidden layers, h = 3 
 nn_instance.addHiddenLayer(1000, activation_fn = tf.nn.relu)
-# add more hidden layers here by calling addHiddenLayer as much as you want
-# a net of depth 3 (3 hidden units) should be sufficient for most tasks
+nn_instance.addHiddenLayer(1000, activation_fn = tf.nn.relu)
+nn_instance.addHiddenLayer(1000, activation_fn = tf.nn.relu)
 
 #Add the output layer of the NN
 nn_instance.addFinalLayer()
+#Make graph connection between predictions and flow graph
+nn_instance.eval()	
 #Setup the computational flow graph 
 #of training using the myNN class 
 nn_instance.setup_training(learn_rate)	
@@ -56,15 +58,18 @@ nn_instance.setup_metrics()
 
 # Training steps	
 with tf.Session() as sess:
+	#Initialise TF local variables used to setup metrics etc
+	sess.run(tf.local_variables_initializer())
 	#Initialize all TF variables used as part of the flow graph
 	sess.run(tf.global_variables_initializer())
 	#Using the currently running session, perform training
-	test_pred = nn_instance.train(sess, xtrain, ytrain, max_epochs, train_size, batch_size) 
+	test_pred = nn_instance.train(sess, xtrain, ytrain, max_epochs, batch_size, train_size, xval, yval, valid_size, xtest) 
 	# fill in other arguments as you modify the train(self, sess, ...) in model.py
 	# you will have to pass xtrain, ytrain, etc ... also as arguments so that you can sample batches in train() of model.py
 
 
 # write code here to store test_pred in relevant file
 out_file_path = "predictions/predictions_mnist.txt"
-out_file = open(out_file_path, w)
-fp.write(test_pred) 	
+#out_file = open(out_file_path, 'w')
+#out_file.write(test_pred) 	
+
